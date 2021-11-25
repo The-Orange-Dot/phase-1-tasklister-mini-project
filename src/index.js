3b3f191ee432a9f5faf4c62b978e1136d9623d1e
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-25 12:00:13 +0900

    Completed, but can't get medium priority task to go between high and low priority

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.querySelector("#new-task-description");
+  const submitBtn = document.querySelector("#submit-button");
+  const todoList = document.querySelector("#tasks");
+  const dropdownMenu = document.querySelector("#dropdown");
+  const div = document.querySelector("#div-container");
+  const low = document.querySelector("#low-priority");
+
+  //---tasks---
+  //Be able to type a task into the input field
+  //Be able to click some form of a submit button
+  //Be able to see the task string to appear in the DOM after submit
+
+  submitBtn.addEventListener("click", (event) => {
+    if (input.value == "") {
+      event.preventDefault();
+    } else {
+      event.preventDefault();
+      const submitted = input.value;
+      let li = document.createElement("LI");
+      let div = document.createElement("DIV");
+      li.innerText = submitted;
+      todoList.appendChild(div);
+      // div.appendChild(li);
+
+      //---addition tasks---
+      //---delete button--
+      let del = document.createElement("button");
+      del.classList.add("del");
+      del.textContent = "Delete";
+      del.style.backgroundColor = null;
+
+      del.addEventListener("click", () => {
+        li.remove();
+        div.remove();
+      });
+
+      //--dropdown color option--
+      if (dropdownMenu.value == "high") {
+        li.style.backgroundColor = "red";
+        li.style.color = "white";
+        div.classList.add("high-priority");
+        todoList.prepend(div);
+        div.append(li);
+        li.append(del);
+      } else if (dropdownMenu.value == "low") {
+        li.style.background = "lightgrey";
+        div.classList.add("low-priority");
+        todoList.appendChild(div);
+        div.append(li);
+        li.append(del);
+      } else {
+        li.style.background = "yellow";
+        div.classList.add("med-priority");
+        todoList.insertBefore(div, low);
+        div.append(li);
+        li.append(del);
+      }
+    }
+  });
 });
